92b0d02b56d3f3a557874a1e89c08ab0e3e3acb6
Lenard Andal <lenardandal@gmail.com>
2009-04-22 15:00:00 -0400

    added test user_spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require File.join( File.dirname(__FILE__), "..", "spec_helper" )
+
+describe User do
+  before :each do
+    # fill me with stuff
+  end
+  
+  it "should be tested"
+  
+  it "needs to be tested" do
+    pending("write some specs")
+  end
+  
+  it "tests like this should not count" do
+    true
+  end
+  
+end
